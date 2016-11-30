9dbfeae3eb73bcbdeb3d904e19c5345216509efa
Andrew Accuardi and Marty Kovach <marty@>
2016-11-30 08:21:01 -0800

    add passing make and model specs for vehicle class.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,10 +1,20 @@
 class Vehicle
   define_method(:initialize) do |make, model, year|
     @year = year
+    @make = make
+    @model = model
   end
 
   define_method(:age) do
     current_year = Time.now().year
     current_year - @year
+  end
+
+  define_method(:make) do
+    @make
+  end
+
+  define_method(:model) do
+    @model
   end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,4 +8,19 @@ describe(Vehicle) do
       expect(test_vehicle.age()).to(eq(16))
     end
   end
+
+  describe("#make") do
+    it("return the make of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.make()).to(eq("Toyota"))
+    end
+  end
+
+  describe("#model") do
+    it("return the model of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.model()).to(eq("Prius"))
+    end
+  end
+
 end
